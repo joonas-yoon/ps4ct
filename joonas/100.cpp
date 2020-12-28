1511f5f65a57a1da41f189e1d00af9fd1f0e7666
Joona Yoon <joonas-yoon@users.noreply.github.com>
2020-12-28 14:43:15 +0900

    프로그래머스 - 삼각 달팽이

--- joonas/100.cpp
+++ joonas/100.cpp
@@ -1,6 +1,32 @@
-#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstring>
 
-int main() {
+using namespace std;
 
-  return 0;
+int a[1001][1001];
+int dy[] = { 1, 0, -1 };
+int dx[] = { 0, 1, -1 };
+
+vector<int> solution(int n) {
+    memset(a, -1, sizeof(a));
+
+    int r = 0, c = 0, l = n - 1, x = 0;
+    a[0][0] = ++x;
+    while (l > 0) {
+        for (int i = 0; i < 3 && l > 0; ++i, --l) {
+            for (int j = 0; j < l; ++j) {
+                r += dy[i];
+                c += dx[i];
+                a[r][c] = ++x;
+            }
+            l += c == 0;
+        }
+    }
+    
+    vector<int> answer;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; a[i][j] != -1; ++j) answer.push_back(a[i][j]);
+    }
+    return answer;
 }
